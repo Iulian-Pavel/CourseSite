8da259886164a928406238651bd5b6aa26ca817e
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2023-03-05 00:33:58 +0200

    changed userpanel heading size

--- src/Components/UserPanel/UserPanel.jsx
+++ src/Components/UserPanel/UserPanel.jsx
@@ -9,11 +9,11 @@ function UserPanel() {
             <div className="left-side">
                 <img src={default_photo} className="default-photo" alt="user profile pic"/>
                 <div className="user-info">
-                  <h2>Username</h2>
+                  <h3>Username</h3>
                   <br />
-                  <h2>Streak: </h2>
+                  <h3>Streak: </h3>
                   <br />
-                  <h2>Streak message</h2>
+                  <h3>Streak message</h3>
                 </div>
             </div>
             <div className="right-side">
